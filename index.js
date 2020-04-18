4f106f6ce14ee2cc6484f37b855c0885efa84248
Rafael de Azevedo <29780126+rdazvd@users.noreply.github.com>
2020-04-18 14:16:17 -0300

    Feat: added video submit event handling This commit also contains a fix in the BrowserWindow instantiation so Node is properly integrated into the application.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const ffmpeg = require('fluent-ffmpeg');
+
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path, (error, metadata) => {
+    console.log('Video duration is:', metadata.format.duration);
+  });
 });
